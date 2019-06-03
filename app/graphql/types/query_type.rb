b1cc6a64541f18fa68532beadd294362a9d3b3df
Bill Fienberg <billfienberg@gmail.com>
2019-06-03 04:23:40 -0500

    Finish step 7, and get tests passing

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,12 +1,5 @@
 module Types
   class QueryType < BaseObject
-    # queries are just represented as fields
-    # `all_links` is automatically camelcased to `allLinks`
-    field :all_links, [LinkType], null: false
-
-    # this method is invoked, when `all_link` fields is being resolved
-    def all_links
-      Link.all
-    end
+    field :all_links, function: Resolvers::LinksSearch
   end
 end
